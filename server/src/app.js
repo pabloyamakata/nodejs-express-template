0487027469f43e2a6ceb79fb100794fe3eec0c5e
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-10-10 15:01:27 -0300

    change import path

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const userRouter = require('./routes/user.js');
+const userRouter = require('./routes/user.routes.js');
 
 app.get('/', (req, res) => {
     res.send(`Requested resource at ${req.url}`);
