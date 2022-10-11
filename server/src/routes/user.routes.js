02e73ae749180aca4321fa5fc70593beb0c3141a
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-10-10 21:01:14 -0300

    WIP

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,10 +1,10 @@
 const { Router } = require('express');
-const { getUsers, getUser } = require('../controllers/user.controller.js');
+const { getUsers, getUser, createUser } = require('../controllers/user.controller.js');
 
 const userRouter = Router();
 
 userRouter.get('/', getUsers);
-
-userRouter.get('/:user', getUser);
+userRouter.get('/:id', getUser);
+userRouter.post('/create', createUser);
 
 module.exports = userRouter;
