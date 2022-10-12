6d4bbd78116f32df0ccbfac176bbcb2810aa172e
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-10-11 23:11:19 -0300

    use patch instead of put

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,19 +1,12 @@
 const { Router } = require('express');
-
-const {
-    getUsers,
-    getUser,
-    createUser,
-    updateUser,
-    deleteUser
-} = require('../controllers/user.controller.js');
+const { getUsers, getUser, createUser, updateUser, deleteUser } = require('../controllers/user.controller.js');
 
 const userRouter = Router();
 
 userRouter.get('/', getUsers);
 userRouter.get('/:id', getUser);
 userRouter.post('/', createUser);
-userRouter.put('/:id', updateUser);
+userRouter.patch('/:id', updateUser);
 userRouter.delete('/:id', deleteUser);
 
 module.exports = userRouter;
