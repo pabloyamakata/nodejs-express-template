9231be62548bb48ce49a006871261ba1d0faf2a1
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-10-13 22:39:04 -0300

    config onDelete behavior

--- server/src/models/User.model.js
+++ server/src/models/User.model.js
@@ -21,7 +21,8 @@ const User = sequelize.define('users', {
 
 User.hasMany(Post, {
     foreignKey: 'userId',
-    sourceKey: 'id'
+    sourceKey: 'id',
+    onDelete: 'CASCADE'
 });
 
 Post.belongsTo(User, {
