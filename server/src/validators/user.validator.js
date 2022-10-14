a719573b7e934ee8564e1e3cde34ee82c20af7d4
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-10-14 03:14:11 -0300

    add validation rules

---
+++ server/src/validators/user.validator.js
@@ -0,0 +1,25 @@
+const { check } = require('express-validator');
+
+const userValidator = [
+    check('username')
+        .exists()
+        .withMessage('Username property must exist')
+        .isString()
+        .withMessage('Username must be a string')
+        .notEmpty()
+        .withMessage('Username cannot be empty'),
+    check('email')
+        .exists()
+        .withMessage('Email property must exist')
+        .isEmail()
+        .withMessage('Input must be of type email')
+        .isLength({ max: 100 })
+        .withMessage('Email cannot contain more than 100 characters'),
+    check('password')
+        .exists()
+        .withMessage('Password property must exist')
+        .isStrongPassword()
+        .withMessage('Password must be at least 8 characters long and contain 1 lowercase letter, 1 uppercase letter, 1 number and 1 symbol')
+];
+
+module.exports = userValidator;
